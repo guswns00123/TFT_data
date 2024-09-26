54e58eb69a104128e143a649aeb4e540deee26ac
Yoo <fresh0911@naver.com>
2024-09-26 22:12:19 +0900

    test

--- dags/dags_tft_api.py
+++ dags/dags_tft_api.py
@@ -1,19 +1,19 @@
-from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+from operators.TFT_api_to_csv_operator import TFTApiToCsvOperator
 from airflow import DAG
 import pendulum
 
 with DAG(
-    dag_id='dags_seoul_api_corona',
+    dag_id='dags_tft_api',
     schedule='0 7 * * *',
     start_date=pendulum.datetime(2023,4,1, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
-    '''서울시 코로나19 확진자 발생동향'''
-    test = SeoulApiToCsvOperator(
+    '''천상계 선수 데이터'''
+    test = TFTApiToCsvOperator(
         task_id='test',
         dataset_nm='league/v1/',
         path='/opt/airflow/files/test/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='test.csv'
+        file_name='sky_user_list.csv'
     )
     
 
--- plugins/operators/TFT_api_to_csv_operator.py
+++ plugins/operators/TFT_api_to_csv_operator.py
@@ -4,7 +4,7 @@ import time
 import requests
 from airflow.models import Variable
 
-class SeoulApiToCsvOperator(BaseOperator):
+class TFTApiToCsvOperator(BaseOperator):
     template_fields = ('path','file_name','base_dt')
 
     def __init__(self, dataset_nm, path, file_name, base_dt=None, **kwargs):
@@ -12,9 +12,8 @@ class SeoulApiToCsvOperator(BaseOperator):
         self.path = path
         self.file_name = file_name
         self.var_value = Variable.get("apikey_tft")
- 
-        
         self.base_dt = base_dt
+
     def execute(self, context):
         import os
 
@@ -29,10 +28,19 @@ class SeoulApiToCsvOperator(BaseOperator):
             else:
                 user_data2 = pd.DataFrame(users)
                 user_data = pd.concat([user_data, user_data2])
-
+        #2
+        #id를 이용하여 puuid 가져와 high_df에 적재
+        high_df = None
+        for index, row in user_data.iterrows():
+            id = row['entries']['summonerId']
+            if high_df is None:
+                high_df = pd.DataFrame(self.extract_game_by_summoner(id, self.base_url))
+            else:
+                high_df2 = pd.DataFrame(self.extract_game_by_summoner(id, self.base_url))
+                high_df = pd.concat([high_df, high_df2])
         if not os.path.exists(self.path):
             os.system(f'mkdir -p {self.path}')
-        user_data.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
+        high_df.to_csv(self.path + '/' + self.file_name, encoding='utf-8', index=False)
 
     def extract_sky(self, tier, base_url):
 
@@ -52,3 +60,17 @@ class SeoulApiToCsvOperator(BaseOperator):
         account_id = requests.get(f"{base_url}{code_name}", headers=request_header).json()
         
         return account_id
+    
+    def extract_game_by_summoner(self, idname, base_url):
+        request_header  = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "https://developer.riotgames.com",
+            "X-Riot-Token": self.var_value
+        }
+        id_code_name = base_url + "league/v1/entries/by-summoner/" + idname
+        user_id = requests.get(id_code_name, headers = request_header).json()
+        time.sleep(2)
+
+        return user_id
