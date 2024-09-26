340d9938167d7fecb78d408ca93ca71135669869
Yoo <fresh0911@naver.com>
2024-09-26 21:22:03 +0900

    test

--- dags/dags_seoul_api_corona.py
+++ dags/dags_seoul_api_corona.py
@@ -9,19 +9,12 @@ with DAG(
     catchup=False
 ) as dag:
     '''서울시 코로나19 확진자 발생동향'''
-    tb_corona19_count_status = SeoulApiToCsvOperator(
-        task_id='tb_corona19_count_status',
+    test = SeoulApiToCsvOperator(
+        task_id='test',
         dataset_nm='league/v1/',
-        path='/opt/airflow/files/TbCorona19CountStatus/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='TbCorona19CountStatus.csv'
+        path='/opt/airflow/files/test/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
+        file_name='test.csv'
     )
     
-    '''서울시 코로나19 백신 예방접종 현황'''
-    tv_corona19_vaccine_stat_new = SeoulApiToCsvOperator(
-        task_id='tv_corona19_vaccine_stat_new',
-        dataset_nm='league/v1/',
-        path='/opt/airflow/files/tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash }}',
-        file_name='tvCorona19VaccinestatNew.csv'
-    )
 
-    tb_corona19_count_status >> tv_corona19_vaccine_stat_new
+    test 
