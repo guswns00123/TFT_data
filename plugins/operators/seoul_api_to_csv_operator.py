091cfe91594d5964b656a9d95307569235ba08eb
Yoo <fresh0911@naver.com>
2024-09-26 21:39:37 +0900

    test

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -2,6 +2,8 @@ from airflow.models.baseoperator import BaseOperator
 import pandas as pd 
 import time
 import requests
+from airflow.models import Variable
+
 class SeoulApiToCsvOperator(BaseOperator):
     template_fields = ('path','file_name','base_dt')
 
@@ -9,13 +11,9 @@ class SeoulApiToCsvOperator(BaseOperator):
         super().__init__(**kwargs)
         self.path = path
         self.file_name = file_name
-        self.request_header  = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
-            "X-Riot-Token": '{{var.value.apikey_tft}}'
-        }
+        self.var_value = Variable.get("apikey_tft")
+ 
+        
         self.base_dt = base_dt
     def execute(self, context):
         import os
@@ -43,8 +41,14 @@ class SeoulApiToCsvOperator(BaseOperator):
             print('해당 함수에서는 master, grandmaster, challenger만 기술할 수 있습니다.')
 
             return None
-
+        request_header  = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
+            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
+            "Origin": "https://developer.riotgames.com",
+            "X-Riot-Token": self.var_value
+        }
         code_name = f"league/v1/{tier}"
-        account_id = requests.get(f"{base_url}{code_name}", headers=self.request_header).json()
+        account_id = requests.get(f"{base_url}{code_name}", headers=request_header).json()
         
         return account_id
