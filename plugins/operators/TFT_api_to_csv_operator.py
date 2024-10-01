f1446d3d1d1f9bc471dd54ae3c671e5f7fdeb42d
hjyoo <fresh0911@naver.com>
2024-10-02 02:15:15 +0900

    bulk load update

--- plugins/operators/TFT_api_to_csv_operator.py
+++ plugins/operators/TFT_api_to_csv_operator.py
@@ -69,17 +69,4 @@ class TFTApiToCsvOperator(BaseOperator):
         
         return account_id
     
-        
-    def extract_game_by_summoner(self, idname, base_url):
-        request_header  = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36",
-            "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
-            "Accept-Charset": "application/x-www-form-urlencoded; charset=UTF-8",
-            "Origin": "https://developer.riotgames.com",
-            "X-Riot-Token": self.test
-        }
-        id_code_name = base_url + "league/v1/entries/by-summoner/" + idname
-        user_id = requests.get(id_code_name, headers = request_header).json()
-        time.sleep(2)
-
-        return user_id
+    
