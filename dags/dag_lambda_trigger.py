fe8cf5e87f52d0d99c0f4c80f03bb958ef212b5c
Yoo <fresh0911@naver.com>
2024-10-09 14:34:41 +0900

    tt

--- dags/dag_lambda_trigger.py
+++ dags/dag_lambda_trigger.py
@@ -8,68 +8,37 @@ import datetime
 import json
 import os
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from airflow.utils.task_group import TaskGroup
+from airflow.decorators import task
 
-def trigger_lambda(file_name,**kwargs):
-    # boto3 클라이언트를 이용한 Lambda 호출
+
+@task
+def list_files_in_s3(bucket_name, prefix):
+    s3_hook = S3Hook('aws_default')
+    files = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix)
+    return files
+
+@task
+def trigger_lambda_batch(file_names):
     session = boto3.Session(
         aws_access_key_id=BaseHook.get_connection('aws_lambda').login,
         aws_secret_access_key=BaseHook.get_connection('aws_lambda').password,
         region_name='ap-northeast-2'
     )
-    payload = {
-        "file_name": file_name  # 파일 이름을 페이로드로 전달
-    }
-    # client = session.client('lambda')
-    # response = client.invoke(
-    #     FunctionName='TFT_data_S3',
-    #     InvocationType='Event',
-    #     Payload=json.dumps(payload).encode('utf-8')
-    # )
-    
-    # # Lambda 함수 실행을 트리거하고 즉시 반환
-    # status_code = response['StatusCode']
-    # if status_code != 202:  # 비동기 호출의 성공 응답은 202 (Accepted)
-    #     raise AirflowFailException(f"Lambda invocation failed with status code {status_code}")
-    
-    # print(f"Lambda function triggered for file: {file_name} asynchronously.")
     client = session.client('lambda')
-    response = client.invoke(
-        FunctionName='TFT_data_S3',
-        InvocationType='RequestResponse',  # 동기 호출로 변경
-        Payload=json.dumps(payload).encode('utf-8')
-    )
-    
-    # Lambda 함수 실행 결과 가져오기
-    response_payload = response['Payload'].read().decode('utf-8')
-    status_code = response['StatusCode']
 
-    if status_code == 200:  # 동기 호출의 성공 응답은 200
-        print(f"Lambda function executed successfully for file: {file_name}")
-        print(f"Response: {response_payload}")
-    else:
-        raise AirflowFailException(f"Lambda invocation failed with status code {status_code}")
-    print(f"Lambda function triggered for file: {response} asynchronously.")
+    for file_name in file_names:
+        payload = {"file_name": file_name}
+        response = client.invoke(
+            FunctionName='TFT_data_S3',
+            InvocationType='Event',
+            Payload=json.dumps(payload).encode('utf-8')
+        )
+        status_code = response['StatusCode']
+        if status_code != 202:
+            raise AirflowFailException(f"Lambda invocation failed for {file_name}")
+        print(f"Triggered Lambda for {file_name}")
 
-# def trigger_lambda2(file_name,**kwargs):
-#     session = boto3.Session(
-#         aws_access_key_id=BaseHook.get_connection('aws_lambda').login,
-#         aws_secret_access_key=BaseHook.get_connection('aws_lambda').password,
-#         region_name='ap-northeast-2'
-#     )
-
-#     client = session.client('lambda')
-#     response = client.invoke(
-#         FunctionName='get_game_result',
-#         InvocationType='Event',
-#         Payload=json.dumps(response).encode('utf-8')
-#     )
-    
-#     # Lambda 함수 실행을 트리거하고 즉시 반환
-#     status_code = response['StatusCode']
-#     if status_code != 202:  # 비동기 호출의 성공 응답은 202 (Accepted)
-#         raise AirflowFailException(f"Lambda invocation failed with status code {status_code}")
-    
-#     print(f"Lambda function triggered for file: {file_name} asynchronously.")
 with DAG(
     dag_id='dag_lambda_trigger',
     start_date=pendulum.datetime(2024,10,1, tz='Asia/Seoul'), 
@@ -81,30 +50,15 @@ with DAG(
         files = s3_hook.list_keys(bucket_name=bucket_name, prefix=prefix)
         return files
 
-    list_files_task = PythonOperator(
-        task_id='list_files',
-        python_callable=list_files_in_s3,
-        op_kwargs={
-            'bucket_name': 'morzibucket',
-            'prefix': 'files/challenger_user_info_batch_'  # 파일 prefix 설정
-        },
-        provide_context=True,
-    )
-    def create_lambda_tasks_from_list(**kwargs):
-        ti = kwargs['ti']
-        file_names = ti.xcom_pull(task_ids='list_files')
-
-        for file_name in file_names:
-            trigger_lambda(file_name) 
-            break # 각 파일에 대해 Lambda 호출
-            
-
-    create_lambda_tasks_op = PythonOperator(
-        task_id='create_lambda_tasks',
-        python_callable=create_lambda_tasks_from_list,
-        provide_context=True,
+    list_files_task = list_files_in_s3(
+        bucket_name='morzibucket',
+        prefix='files/challenger_user_info_batch_'
     )
 
+    # Step 2: 동적으로 3개의 파일씩 Lambda 호출
+    trigger_lambdas_task = trigger_lambda_batch.expand(
+        file_names=list_files_task[:3]  # 3개씩 그룹화하여 실행
+    )
 
+    list_files_task >> trigger_lambdas_task
 
-    list_files_task >> create_lambda_tasks_op 
