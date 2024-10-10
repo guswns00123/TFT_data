be8a2ce14eaf4b95a0c59622fac1920247f193f0
Yoo <fresh0911@naver.com>
2024-10-10 17:11:48 +0900

    tt

---
+++ dags/dags_TFT_game_hook_bulk_load.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.python import PythonOperator
+from hooks.custom_postgres_hook import CustomPostgresHook
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.operators.email import EmailOperator
+from airflow.operators.empty import EmptyOperator
+
+# 챌린저 유저 정보 데이터를 local Postgres DB에 적재하는 DAG
+with DAG(
+        dag_id='dags_TFT_game_hook_bulk_load',
+        start_date=pendulum.datetime(2024, 10, 1, tz='Asia/Seoul'),
+        schedule=None,
+        catchup=False
+) as dag:
+    
+    start = EmptyOperator(
+    task_id='start'
+    )
+
+    def insrt_postgres(postgres_conn_id, tbl_nm, file_nm, **kwargs):
+        custom_postgres_hook = CustomPostgresHook(postgres_conn_id=postgres_conn_id)
+        custom_postgres_hook.bulk_load(table_name=tbl_nm, file_name=file_nm, delimiter=',', is_header=True, is_replace=True)
+
+    
+    insrt_postgres1 = PythonOperator(
+        task_id='insrt_postgres',
+        python_callable=insrt_postgres,
+        op_kwargs={'postgres_conn_id': 'conn-db-postgres-custom',
+                   'tbl_nm':'tft_game_info',
+                   'file_nm':'/opt/airflow/files/game_res/challenger_game_res_1.csv'}
+    )
+
+    start >> insrt_postgres1
