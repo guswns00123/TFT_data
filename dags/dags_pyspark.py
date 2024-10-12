ab4a5e6ddf375449fa8c14524dea5a2fab0ad871
Yoo <fresh0911@naver.com>
2024-10-12 14:46:00 +0900

    tt

--- dags/dags_pyspark.py
+++ dags/dags_pyspark.py
@@ -1,24 +1,13 @@
 from airflow import DAG
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from datetime import datetime, timedelta
+import pendulum
 
-# 기본 설정
-default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2024, 10, 12),
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
-}
-
-# DAG 정의
 with DAG(
-    dag_id='dags_pyspark',
-    default_args=default_args,
-    schedule_interval=None,  # 실행 주기 설정
-    catchup=False,
+    dag_id="dags_pyspark",
+    schedule=None,
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
 ) as dag:
 
     # PySpark 작업 실행
